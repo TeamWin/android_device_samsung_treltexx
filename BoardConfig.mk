7e396202de1b89c135990acb14f6839d474f9d20
James Christopher Adduono <jc@adduono.com>
2016-12-05 19:12:07 -0500

    Update device for Android 6.0
    Change-Id: I85eea18356f400e51d107c47316ba3f37254a9bb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,72 +1,89 @@
-USE_CAMERA_STUB := true
+DEVICE_TREE := device/samsung/treltexx
 
-# inherit from the proprietary version
--include vendor/samsung/treltexx/BoardConfigVendor.mk
-
+# Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := universal5433
 
 # Platform
-TARGET_BOARD_PLATFORM := exynos5433
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_BOARD_PLATFORM_GPU := mali-t760mp6
 
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DNO_SECURE_DISCARD
 
 # Architecture
-TARGET_CPU_VARIANT := cortex-a15
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
-BOARD_KERNEL_CMDLINE :=
-BOARD_KERNEL_BASE :=  0x10000000
+# Kernel
+#TARGET_KERNEL_SOURCE := kernel/samsung/universal5433
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_CONFIG := twrp_defconfig
+TARGET_KERNEL_DEVICE_DEFCONFIG := device_trelte_xx
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/zImage
+
+# DTB
+TARGET_DTBH_PLATFORM_CODE := 0x000050a6
+TARGET_DTBH_SUBTYPE_CODE  := 0x217584da
+TARGET_PREBUILT_DTB := $(DEVICE_TREE)/dtb.img
+
+# Boot image
+BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
+BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/treltexx/dtb
+# 000RU = recovery kernel, 000KU = system kernel
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000RU
+BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/bootimg.mk
 
-BOARD_BOOTIMAGE_PARTITION_SIZE :=     0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00D00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x000E00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x001000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0E1000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x6453FC000 # 0x645400000 - 16384 (crypto footer)
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x00C800000
+BOARD_HIDDENIMAGE_PARTITION_SIZE   := 0x008200000
+BOARD_FLASH_BLOCK_SIZE := 0x20000
 
-TARGET_PREBUILT_KERNEL := device/samsung/treltexx/kernAl
-
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
-
-# Use this flag if the board has a ext4 partition larger than 2gb
+# File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
-DEVICE_RESOLUTION := 1440x2560
+TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TW_SCREEN_BLANK_ON_BOOT := true
-#TW_NO_SCREEN_BLANK := false
-#TW_BRIGHTNESS_PATH := /sys/devices/platform/s5p-mipi-dsim.1/backlight/panel
-TW_BRIGHTNESS_PATH := /sys/devices/13800000.decon_fb/backlight/panel/brightness
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/15400000.usb/15400000.dwc3/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/13800000.decon_fb/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-#TW_INCLUDE_CRYPTO := true
-#TW_INCLUDE_CRYPTO_SAMSUNG := true
-#TW_CRYPTO_FS_TYPE := "ext4"
-#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
-#TW_CRYPTO_MNT_POINT := "/data"
-#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic"
-#TW_CRYPTO_FS_FLAGS := "0x00000406"
-#TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_NTFS_3G := true
+
+# exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
-TW_NO_EXFAT := true
+
+# No love for the wicked (Android M incompatible)
+TW_EXCLUDE_SUPERSU := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := true
+
+# Init properties from bootloader version, ex. model info
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_treltexx
+TARGET_RECOVERY_DEVICE_MODULES := libinit_treltexx
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_treltexx.cpp
