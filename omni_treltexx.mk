a396b89de5f1fc6f9b560438222b4e25f4a8e88e
ananjaser1211 <ananjaser@gmail.com>
2018-09-04 23:42:10 +0400

    Remove some stuff
    For smaller size

--- omni_treltexx.mk
+++ omni_treltexx.mk
@@ -19,14 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
-
-# Time Zone data for Recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 	
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := treltexx
